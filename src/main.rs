73d3157f654d4122ffae7d2f43f1ddc6ca46288d
Michal Kosairz <michal.kosiarz@gmail.com>
2024-10-29 21:30:19 +0100

    Add implementation for transmit thread
    Add transmit thread implementation including synchronization with main thread.
    Add socket connection and transmission (send and receive).
    Add constant for some values.
    
    Signed-off-by: Michal Kosairz <michal.kosiarz@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use clap::Parser;
-
 use core::fmt;
 #[allow(unused_imports)]
 use std::net::{SocketAddr, UdpSocket, IpAddr, Ipv4Addr};
@@ -12,9 +11,10 @@ use std::time::{Duration, Instant};
 use crossterm::event::{KeyCode, Event};
 use crossterm::event;
 
+pub const TICK_FOR_OPERATION_TIMEOUT: u64 = 10;
+
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None, author = "MK")]
-
 struct Args {
     /// specifies if send thread needs to be started
     #[arg(short, long, group="direction")]
@@ -79,22 +79,37 @@ impl App{
     fn new() -> App {
         App {
             dir: Direction::None,
-            ip_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 8080),
+            ip_addr: SocketAddr::new(IpAddr::V4(Ipv4Addr::new(127, 0, 0, 1)), 62345),
             receive_timeout: 10,
         }
     }
 
-    fn transmit_thread() {
-        println!("Transmiting ....");
-    }
-
-    fn receive_thread(msg_to_me: &std::sync::mpsc::Receiver<String>, to_main: &std::sync::mpsc::SyncSender<String>, timeout: u32) {
+    fn transmit_thread(msg_to_me: &std::sync::mpsc::Receiver<String>, to_main: &std::sync::mpsc::SyncSender<String>, timeout: u32, ip_address: SocketAddr) {
         let start = Instant::now();
         let time_out = Duration::from_secs(timeout as u64);
+        let mut count = 0;
 
-        println!("Receiving ....");
+        println!("Transmiting ....");
+
+        let socket:UdpSocket;
+        let mut tx_sock_addr = ip_address.clone();
+        tx_sock_addr.set_port(tx_sock_addr.port() + 1);
+
+        match UdpSocket::bind(tx_sock_addr) {
+            Ok(sock) => socket = sock,
+            Err (_) => {
+                println!("Cannot bind to the socket");
+                println!("Time spent in thread: {:?}", start.elapsed());
+                to_main.send("quiting".to_string()).unwrap();
+                return;        
+            }
+        };
+
+        // socket.set_broadcast(true).expect("Failed to set broadcast");
+        socket.connect(ip_address).unwrap();
+
         'klop: loop {
-            match msg_to_me.recv_timeout(Duration::from_millis(10)) {
+            match msg_to_me.recv_timeout(Duration::from_millis(TICK_FOR_OPERATION_TIMEOUT)) {
                 Ok(message) => {
                     if message.contains("quit") {
                         break;
@@ -102,7 +117,69 @@ impl App{
                 }
                 _ => {}
             }
+        
+            if timeout != 0 && start.elapsed() >= time_out {
+                break 'klop;
+            }
+        
+            let mut message: Vec<u8> = (format!("$SCP,0,1,{}", count)).into_bytes().to_vec();
+            println!("Sedning message: {:?}", message);
+            count += 1;
+            match socket.send(&mut message) {
+                Ok(_) => {}
+                Err(e) => println!("packtet {} dropped with error: {:?}", count - 1, e),
+            }
+            sleep(Duration::from_secs(1));
+        }
 
+        println!("Time spent in thread: {:?}", start.elapsed());
+        to_main.send("quiting".to_string()).unwrap();
+    }
+
+    fn receive_thread(msg_to_me: &std::sync::mpsc::Receiver<String>, to_main: &std::sync::mpsc::SyncSender<String>, timeout: u32, ip_address: SocketAddr) {
+        let start = Instant::now();
+        let time_out = Duration::from_secs(timeout as u64);
+
+        println!("Receiving ....");
+
+        let socket:UdpSocket;
+        let mut tx_socket = ip_address;
+        tx_socket.set_port(tx_socket.port() + 1);
+
+        match UdpSocket::bind(ip_address) {
+            Ok(sock) => socket = sock,
+            Err (_) => {
+                println!("Cannot bind to the socket");
+                println!("Time spent in thread: {:?}", start.elapsed());
+                to_main.send("quiting".to_string()).unwrap();
+                return;        
+            }
+        };
+        socket.set_read_timeout(Some(Duration::from_millis(TICK_FOR_OPERATION_TIMEOUT))).unwrap();
+        socket.connect(tx_socket).unwrap();
+
+        let mut rx_buf = vec![0u8; 4096];
+        rx_buf.clear();
+
+        'klop: loop {
+            match msg_to_me.recv_timeout(Duration::from_millis(TICK_FOR_OPERATION_TIMEOUT)) {
+                Ok(message) => {
+                    if message.contains("quit") {
+                        break;
+                    }        
+                }
+                _ => {}
+            }
+        
+            match socket.recv(& mut rx_buf) {
+                Ok(size) => {
+                    if size != 0 {
+                        println!("Received: {:?}", rx_buf);
+                        rx_buf.clear();
+                    }
+                }
+                Err(_) => {}
+            }
             if timeout != 0 && start.elapsed() >= time_out {
                 break 'klop;
             }
@@ -119,29 +196,33 @@ impl App{
         let transmit_handle: std::thread::JoinHandle<()>;
         let (to_receiver, for_receiver) = mpsc::sync_channel::<String>(100);
         let (form_threads, to_main) = mpsc::sync_channel::<String>(100);
+        let (to_transmiter, for_transmiter) = mpsc::sync_channel::<String>(100);
 
-//        let tramsm2 = trnsm.clone();
+        let form_threads2 = form_threads.clone();
 
         if self.dir == Direction::Receive || self.dir == Direction::SendReceive {
             println!("Starting receiving thread ....");
-            receive_handle = std::thread::spawn(move || { Self::receive_thread(&for_receiver, &form_threads, self.receive_timeout) });
+            receive_handle = std::thread::spawn(move || { Self::receive_thread(&for_receiver, &form_threads, self.receive_timeout, self.ip_addr) });
         } else {
             receive_handle = std::thread::spawn(move || { });
         }
 
         if self.dir == Direction::Send || self.dir == Direction::SendReceive {
             println!("Starting transmiting thread ....");
-            transmit_handle = std::thread::spawn(move || { Self::transmit_thread() });
+            transmit_handle = std::thread::spawn(move || { Self::transmit_thread(&for_transmiter, &form_threads2, self.receive_timeout, self.ip_addr) });
         } else {
             transmit_handle = std::thread::spawn(move || { });
         }
 
         'ThreadLoop: loop {
-            if event::poll(Duration::from_millis(10)).unwrap() {
+            if event::poll(Duration::from_millis(TICK_FOR_OPERATION_TIMEOUT)).unwrap() {
                 if let Ok(Event::Key(key)) = event::read() {
                     match key.code {
                         KeyCode::Char('q') => {
                             match to_receiver.send("quit".to_string()) {
+                                _ => {}
+                            }
+                            match to_transmiter.send("quit".to_string()) {
                                 _ => {}
                             }
                             break 'ThreadLoop;
